cae6779a5cb8dd5d17004de3236a0af857d43aa0
tfrvld <tfrvld@gmail.com>
2024-07-30 15:43:15 +0700

    update darkmode dan perbaikan fitur

--- dist/script.js
+++ dist/script.js
@@ -27,8 +27,29 @@ hamburger.addEventListener('click', function () {
 
 //? klik di luar hamburger
 window.addEventListener("click", function (e) {
-   if (e.target != hamburger && e.target != navMenu) {
+   if (!hamburger.contains(e.target) && !navMenu.contains(e.target)) {
       hamburger.classList.remove('hamburger-active');
       navMenu.classList.add('hidden');
    }
 });
+
+//? Darkmode toggle
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click', function () {
+   if (darkToggle.checked) {
+      html.classList.add('dark');
+      localStorage.theme = 'dark';
+   } else {
+      html.classList.remove('dark');
+      localStorage.theme = 'light';
+   }
+})
+
+//? pindahkan posisi toggle sesuai mode
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+   darkToggle.checked = true;
+} else {
+   darkToggle.checked = false;
+}
